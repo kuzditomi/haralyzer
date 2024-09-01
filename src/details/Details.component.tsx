915e1f19881bd8cbb229aaaf31664f2d4ae29feb
kuzditomi <kuzditomi@gmail.com>
2024-09-01 13:45:46 +0200

    response

--- src/details/Details.component.tsx
+++ src/details/Details.component.tsx
@@ -1,13 +1,48 @@
 import { FC } from 'react';
 import { useRecoilValue } from 'recoil';
 import { selectedHarEntry } from '../har-management/har.state';
+import './details.scss';
+import classNames from 'classnames';
 
 export const DetailsComponent: FC = () => {
-  const selectedEntry = useRecoilValue(selectedHarEntry);
+  const entry = useRecoilValue(selectedHarEntry);
 
-  if (!selectedEntry) {
+  if (!entry) {
     return <div />;
   }
 
-  return <div>{selectedEntry.request.url}</div>;
+  return (
+    <div className="details">
+      <div className="request">
+        <h3>Request</h3>
+        <p>{entry.request.url}</p>
+
+        <h3>Headers</h3>
+        <ul>
+          {entry.request.headers.map((header) => (
+            <li>
+              <label>{header.name}:</label>
+              <p>{header.value}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="response">
+        <h3>Response</h3>
+        <p
+          className={classNames({
+            success: entry.response.status < 300,
+            warn: entry.response.status >= 300 && entry.response.status < 400,
+            error: entry.response.status > 400,
+          })}
+        >
+          {entry.response.status}
+          {entry.response.statusText ? ' - ' + entry.response.statusText : ''}
+        </p>
+
+        <h3>Body</h3>
+        <pre>{JSON.stringify(entry.response.content, null, 2)}</pre>
+      </div>
+    </div>
+  );
 };
